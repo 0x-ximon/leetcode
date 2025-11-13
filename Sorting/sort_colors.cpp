e9b72826318e4940ca86c488eee8430faa3c8546
Simon Samuel <56292632+0x-ximon@users.noreply.github.com>
2025-11-13 08:38:26 +0100

    feat: solved sort colors problem

---
+++ Sorting/sort_colors.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    public:
+    void sortColors(vector < int>& nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            int m = i;
+            
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[j] < nums[m]) m = j;
+            }
+            
+            swap(nums[i], nums[m]);
+        }
+    }
+};
