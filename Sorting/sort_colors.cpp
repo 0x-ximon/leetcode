632cc360ed776412f45b6280e4b25fc946a6006f
Simon Samuel <salviensky@gmail.com>
2025-11-14 08:32:30 +0100

    chore: formated sort_colors problem

--- Sorting/sort_colors.cpp
+++ Sorting/sort_colors.cpp
@@ -1,14 +1,16 @@
+#include <vector>
+
 class Solution {
-    public:
-    void sortColors(vector < int>& nums) {
+   public:
+    void sortColors(std::vector<int>& nums) {
         for (int i = 0; i < nums.size(); i++) {
             int m = i;
-            
+
             for (int j = i + 1; j < nums.size(); j++) {
                 if (nums[j] < nums[m]) m = j;
             }
-            
-            swap(nums[i], nums[m]);
+
+            std::swap(nums[i], nums[m]);
         }
     }
 };
