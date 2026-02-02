829355d4b2dbca1fffb85bdeff0cca0d3ad4e217
Simon Samuel <salviensky@gmail.com>
2026-02-02 08:34:25 +0100

    feat: gave up and solved kth largest via sorting

---
+++ Sorting/kth_largest_element_in_an_array.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+class Solution {
+   public:
+    int findKthLargest(std::vector<int>& nums, int k) {
+        std::sort(nums.begin(), nums.end());
+        auto n = nums.size();
+        return nums[n - k];
+    }
+};
